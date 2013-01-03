675c3d99b23f8072409395fbe98aedbfea215420
Joe Fearnley <joe.fearnley@gmail.com>
2013-01-02 22:12:02 -0500

    Realizing what the backbone.view.tamplate attribute does.

--- js/app.js
+++ js/app.js
@@ -3,21 +3,11 @@
  *
  * @author Joe Fearnley
  */
-
 (function($){
-
-  var flickrSearchApi = {
-    key: '802074e4a7efce70a34ac2e985cc928d',
-    userId: '19111910%40N00',
-    method: 'flickr.photos.search',
-    tag: 'elcheatum',
-    baseUrl: 'http://api.flickr.com/services/rest/'
-  };
 
   var Photo = Backbone.Model.extend({});
 
   var PhotoList = Backbone.Collection.extend({
-    model: Photo,
     url: 'http://api.flickr.com/services/feeds/photos_public.gne?tags=elcheatum&tagmode=any&format=json&jsoncallback=?',
     parse: function(response) {
       return response.items;
@@ -26,13 +16,11 @@
 
   var PhotoView = Backbone.View.extend({
     el: $('#photos'),
-    template: $('#photo-template'),
+    template: $('#photo-template').html(),
     initialize: function() {
       this.collection = new PhotoList();
-
       this.render();
     },
-
     render: function() {
       var that = this;
       this.collection.fetch({
@@ -48,7 +36,6 @@
           console.log('collection fetch was a failure');
         }
       });
-
     },
     renderPhoto: function(photo) {
       var html = Mustache.to_html(this.template, photo);
